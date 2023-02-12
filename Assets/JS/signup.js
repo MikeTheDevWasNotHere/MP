3e87ec835b498947756d0b428209d62db0fbe399
Misha76452 <119761148+Misha76452@users.noreply.github.com>
2023-02-12 14:36:39 -0800

    2

--- Assets/JS/signup.js
+++ Assets/JS/signup.js
@@ -2,23 +2,52 @@ function checkPassword(form) {
     password1 = form.password1.value;
     password2 = form.password2.value;
 
-    // If password not entered
     if (password1 == '')
-        alert ("Please enter Password");
+        alert ("Please enter a password.");
             
-    // If confirm password not entered
     else if (password2 == '')
-        alert ("Please enter confirm password");
+        alert ("Please confirm your password.");
             
     // If Not same return False.    
     else if (password1 != password2) {
-        alert ("\nPassword did not match: Please try again...")
+        alert ("\nThe password you entered did not match the comfirmiation password. Please try again...")
         return false;
     }
 
     // If same return True.
     else {
-        alert("Password Match: Welcome to GeeksforGeeks!")
+        alert("Your account has been created! Please login to activate your account.")
         return true;
+
+        const name = document.getElementById('txtName');
+
+        let Weekday = Date.prototype.getDay() + 1;
+        let Month = Date.prototype.getMonth() + 1;
+        let Day = Date.prototype.getDate();
+        let Hour = Date.prototype.getHours() + 1;
+        let Minutes = Date.prototype.getMinutes() + 1;
+        let Seconds = Date.prototype.getSeconds() + 1;
+        let MilliSec = Date.prototype.getMilliseconds();
+
+        let ID = Weekday + Month + Day + Hour + Minutes + Seconds + MilliSec;
+
+        let data = 
+            `
+                {
+                    "username": "${name.value}",
+                    "password": "${pass.value}",
+                    "picture": "${pic.value}",
+                    "ID": "${ID}"
+                },
+            `
+
+        const fs = require('fs');
+        
+        fs.writeFile('MP/Database/Accounts/accounts.js', data, err => {
+            if (err) {
+            console.error(err);
+            }
+            // file written successfully
+        });
     }
 }
